024bc2cbc1b4a92eae2fa82458109eee0e639d45
MeanJR <70934291+MeanJR@users.noreply.github.com>
2022-01-04 12:07:41 +0700

    Add files via upload

---
+++ GradeCalculate/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace GradeCalculate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int score = int.Parse(Console.ReadLine());
+            String grade = (score >= 80) ? "A" :
+                           (score >= 70) ? "B" :
+                           (score >= 60) ? "C" :
+                           (score >= 50) ? "D" :
+                                           "F" ;
+            Console.WriteLine(grade);
+        }
+    }
+}
